488c2738cc52690abe7d55d363e510f9aebd5e32
Ummah571 <samandaragzamov1@gmail.com>
2022-01-19 08:03:17 +0300

    first commit

---
+++ Scirpts/script.js
@@ -0,0 +1,13 @@
+var myname = prompt("Введите свое Имя")
+
+if (myname === "AbdulMalik") {
+    alert("Здраствуй Босс")
+} else if (myname === "АбдулМалик") {
+    alert("Здраствуй Босс")
+} else if (myname === "Abdulmalik") {
+    alert("Здраствуй Босс")
+} else if (myname === "Абдулмалик") {
+    alert("Здраствуй Босс")
+} else{
+    alert("Привет Гость")
+}
